368423cfd338733bdaba461b51d75f12b068abc4
pedrotunin <pedrotunin@gmail.com>
2018-09-28 21:46:01 -0300

    vimrc

---
+++ vimrc.cpp
@@ -0,0 +1,11 @@
+set number		
+set showmatch		
+set background=dark
+ 
+set autoindent		
+set smartindent
+set shiftwidth=4
+set softtabstop=4
+set tabstop=4
+
+au BufNewFile ~/*.cpp 0r ~/base.cpp     
