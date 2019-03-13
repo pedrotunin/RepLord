2cba9fb87d9eb1918e1aa1d2a9119553f183c6e7
pedrotunin <pedrotunin@gmail.com>
2019-03-12 22:46:39 -0300

    teste

---
+++ teste.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sec second
+#define endl '\n'
+
+typedef long long ll;
+const double PI = acos(-1.0);
+
+int main(){
+	ios_base::sync_with_stdio(false);
+
+	
+
+	return 0;
+}
+
+
